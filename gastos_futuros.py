72f91a858d4730a8775c7241e7ff243aed1fc7b7
cantaruttim <cantaruttim@outlook.com>
2025-09-08 22:28:09 -0300

    updated

--- gastos_futuros.py
+++ gastos_futuros.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from datetime import datetime
+
+hoje = datetime.today()
+ano_mes_atual = hoje.strftime('%Y-%m')
 
 donos = pd.read_excel("./dados/Controle de Gastos.xlsx", sheet_name="Gastos_2025")
 gf = pd.read_excel("./dados/parcelado.xlsx", sheet_name="parcelados")
@@ -24,17 +28,13 @@ def tras_dono_cartao(gf, donos):
 
     # Faz o merge com os donos passados como argumento
     df22 = df22.merge(donos.drop_duplicates(), on='Cartão', how='left')
-    df22 = df22[ df22["Dono"].isin(["GABRIELLA QUINTEIRO", "MATHEUS CANTARUTTI"])]
+    df22 = df22[ df22["Dono"].isin(
+        ["GABRIELLA QUINTEIRO", "MATHEUS CANTARUTTI"]
+        )
+    ]
+    df22 = df22.drop(columns=["index"])
     return df22
 
-
-# Chamada da função corrigida
 df = tras_dono_cartao(gf, donos[["Cartão", "Dono"]])
 
-# Certifique-se de que 'Vigência' é datetime
-df['Vigência'] = pd.to_datetime(df['Vigência'], errors='coerce')
-
-# Cria uma coluna de Ano-Mês da próxima parcela
-df['AnoMes'] = df['Vigência'] + pd.DateOffset(months=1)
-df['AnoMes'] = df['AnoMes'].dt.to_period('M')
-
+print(df)
