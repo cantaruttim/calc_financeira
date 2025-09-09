d3e5051808085a045d7677a8fd5bac0b07869762
cantaruttim <cantaruttim@outlook.com>
2025-09-08 23:13:39 -0300

    updated

--- gastos_futuros.py
+++ gastos_futuros.py
@@ -7,6 +7,15 @@ ano_mes_atual = hoje.strftime('%Y-%m')
 donos = pd.read_excel("./dados/Controle de Gastos.xlsx", sheet_name="Gastos_2025")
 gf = pd.read_excel("./dados/parcelado.xlsx", sheet_name="parcelados")
 
+def parcelas_pendentes(df):
+    # Garante tipo numérico
+    df['PARCELA'] = pd.to_numeric(df['PARCELA'], errors='coerce')
+    df['TOTAL_PARCELA'] = pd.to_numeric(df['TOTAL_PARCELA'], errors='coerce')
+
+    # Calcula parcelas pendentes
+    df['parcelas_pendentes'] = df['TOTAL_PARCELA'] - df['PARCELA']
+    return df
+
 def retorna_status_parcela(df):
     if (df["PARCELA"] <= df["TOTAL_PARCELA"]).all():
         df["proxima_parcela"] = "Possui"
@@ -19,8 +28,8 @@ def retorna_maior_anomes(df):
     maior_anomes = df["ANOMES"].max()
     return df[ df["ANOMES"] == maior_anomes ]
 
-def tras_dono_cartao(gf, donos):
-    df22 = gf.copy()
+def tras_dono_cartao(df, donos):
+    df22 = df.copy()
 
     if isinstance(df22.columns, pd.MultiIndex):
         df22.columns = ['_'.join(col).strip() if isinstance(col, tuple) else col for col in df22.columns]
@@ -42,11 +51,22 @@ def tras_dono_cartao(gf, donos):
     df22 = df22.drop(columns=["index"])
     return df22
 
+def valor_pendente(df):
+    df['VALOR'] = pd.to_numeric(df['VALOR'], errors='coerce')
+    df['TOTAL_PARCELA'] = pd.to_numeric(df['TOTAL_PARCELA'], errors='coerce')
+    df['PARCELA'] = pd.to_numeric(df['PARCELA'], errors='coerce')
+
+    df['valor_pendente'] = df["parcelas_pendentes"] * df["VALOR"]
+    return df
+
+
 df = tras_dono_cartao(gf, donos[["Cartão", "Dono"]])
 df = retorna_maior_anomes(df)
 df = retorna_status_parcela(df)
+df = parcelas_pendentes(df)
+df = valor_pendente(df)
 
 
+df2 = df.groupby("ANOMES").sum(numeric_only=True)
 
-
-print(df)
+print(df2)
